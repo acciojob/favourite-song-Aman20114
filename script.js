7796838184228d7d9f26aed5851af55f8316918b
Aman Tiwari <110708815+Aman20114@users.noreply.github.com>
2023-02-21 21:10:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your code here
+const songName = "Bohemian Rhapsody";
+const youtubeLink = "https://www.youtube.com/watch?v=fJ9rUzIMcZQ";
+
+const div = document.createElement("div");
+const text = document.createTextNode(`My current favourite song is ${songName}`);
+const link = document.createElement("a");
+link.setAttribute("href", youtubeLink);
+link.appendChild(text);
+div.appendChild(link);
+
+document.body.appendChild(div);
